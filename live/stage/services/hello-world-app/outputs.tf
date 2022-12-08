ee681a62f9f40cb9aa1197ac0de4caf97fa3309b
ptisma <ptisma@petartisma.localdomain>
2022-12-08 12:35:36 +0100

    test

--- live/stage/services/hello-world-app/outputs.tf
+++ live/stage/services/hello-world-app/outputs.tf
@@ -4,7 +4,8 @@ output "alb_dns_name" {
 }
 
 output "values" {
-  value       = nonsensitive(module.hello_world_app.values)
+  //value       = nonsensitive(module.hello_world_app.values)
+  value       = module.hello_world_app.values
   description = "test"
   //sensitive = true
 }
