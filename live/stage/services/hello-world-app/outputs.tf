1670c068032152c8cde64b481ca21d3523f42403
ptisma <ptisma@petartisma.localdomain>
2022-12-08 11:52:48 +0100

    add output to workdirectory

--- live/stage/services/hello-world-app/outputs.tf
+++ live/stage/services/hello-world-app/outputs.tf
@@ -2,3 +2,8 @@ output "alb_dns_name" {
   value       = module.hello_world_app.alb_dns_name
   description = "The domain name of the load balancer"
 }
+
+output "test" {
+  value       = module.hello_world_app.tfe_outputs
+  description = "test"
+}
